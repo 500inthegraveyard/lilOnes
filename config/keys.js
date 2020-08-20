f45c88f31d016240a07658beef4ce2c4207dc7f7
Mike Marien <mikemarien@protonmail.ch>
2020-08-20 13:39:13 -0500

    route changes

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://mikemarien:'+ encodeURIComponent('extraconsiderations') + '@cluster0-iitbm.mongodb.net/users?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
